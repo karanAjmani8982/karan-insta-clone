5e6228ef6046d95cd99746f630d357c64240ce71
karanAjmani8982 <karanajmani72@gmail.com>
2022-02-16 14:46:41 +0530

    removed images

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,51 +5,51 @@ exports.createPost = (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
+    //  imagePath: url + "/images/" + req.file.filename,
+    creator: req.userData.userId,
   });
   post
     .save()
-    .then(createdPost => {
+    .then((createdPost) => {
       res.status(201).json({
         message: "Post added successfully",
         post: {
           ...createdPost,
-          id: createdPost._id
-        }
+          id: createdPost._id,
+        },
       });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: "Creating a post failed!"
+        message: "Creating a post failed!",
       });
     });
 };
 
 exports.updatePost = (req, res, next) => {
-  let imagePath = req.body.imagePath;
+  // let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + "://" + req.get("host");
-    imagePath = url + "/images/" + req.file.filename;
+    //   imagePath = url + "/images/" + req.file.filename;
   }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
-    imagePath: imagePath,
-    creator: req.userData.userId
+    // imagePath: imagePath,
+    creator: req.userData.userId,
   });
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-    .then(result => {
+    .then((result) => {
       if (result.n > 0) {
         res.status(200).json({ message: "Update successful!" });
       } else {
         res.status(401).json({ message: "Not authorized!" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: "Couldn't udpate post!"
+        message: "Couldn't udpate post!",
       });
     });
 };
@@ -63,43 +63,43 @@ exports.getPosts = (req, res, next) => {
     postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
   postQuery
-    .then(documents => {
+    .then((documents) => {
       fetchedPosts = documents;
       return Post.count();
     })
-    .then(count => {
+    .then((count) => {
       res.status(200).json({
         message: "Posts fetched successfully!",
         posts: fetchedPosts,
-        maxPosts: count
+        maxPosts: count,
       });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: "Fetching posts failed!"
+        message: "Fetching posts failed!",
       });
     });
 };
 
 exports.getPost = (req, res, next) => {
   Post.findById(req.params.id)
-    .then(post => {
+    .then((post) => {
       if (post) {
         res.status(200).json(post);
       } else {
         res.status(404).json({ message: "Post not found!" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: "Fetching post failed!"
+        message: "Fetching post failed!",
       });
     });
 };
 
 exports.deletePost = (req, res, next) => {
   Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
-    .then(result => {
+    .then((result) => {
       console.log(result);
       if (result.n > 0) {
         res.status(200).json({ message: "Deletion successful!" });
@@ -107,9 +107,9 @@ exports.deletePost = (req, res, next) => {
         res.status(401).json({ message: "Not authorized!" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: "Deleting posts failed!"
+        message: "Deleting posts failed!",
       });
     });
 };
